e51a929d7575c56d2661d4de60ca7436af9d983c
Ethan Yang <eyang182@gmail.com>
2025-01-02 21:56:43 -0800

    feat: fix project data and portfolio page

--- milestone-2/src/app/portfolio/page.tsx
+++ milestone-2/src/app/portfolio/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import connectDB from "../database/db"
 import Project from "../database/projectSchema";
 import ProjectPreview from "../components/projectPreview";
+import projects from "../projectData"
 
 async function getProjects(){
   await connectDB() 
@@ -15,22 +16,17 @@ async function getProjects(){
   }
 }
 
-type ProjectProps = {
-  projects: Project[];
-};
-
-export default function Projects({ projects }: ProjectProps) {
-  return (
-    <div className="projects">
-      {projects.map((project) => (
-        <ProjectPreview
-          key={project.slug} // Ensure unique key
-          title={project.title}
-          description={project.description}
-          image={project.image}
-          slug={project.slug}
-        />
-      ))}
-    </div>
+export default function Projects(props: Project){
+  return(
+  <div className = 'projects'>
+        {projects.map(project => 
+    <ProjectPreview 
+    title={project.title}
+    description={project.description}
+    image={project.image}
+    slug={project.slug}
+    /> 
+  )}
+  </div>
   );
 }
