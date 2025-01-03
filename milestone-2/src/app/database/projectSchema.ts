4b9d805f541d97e122b40196d6407491e21fc4c1
Ethan Yang <eyang182@gmail.com>
2025-01-02 21:20:07 -0800

    feat: update projectSchema template

--- milestone-2/src/app/database/projectSchema.ts
+++ milestone-2/src/app/database/projectSchema.ts
@@ -2,16 +2,16 @@ import mongoose, { Schema } from "mongoose";
 
 type Project = {
         title: string;
-        slug: string; 
         description: string; 
         image: string; // url for string in public
+        slug: string; 
 };
 
 const projectSchema = new Schema<Project>({
         title: { type: String, required: true },
-        slug: { type: String, required: true },
         description: { type: String, required: true },
         image: { type: String, required: true },
+        slug: { type: String, required: true },
 })
 
 const Project = mongoose.models['projects'] ||
