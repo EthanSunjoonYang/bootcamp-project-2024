f3fb235378bc8a7d05a354f322f940fa71e72004
Ethan Yang <eyang182@gmail.com>
2025-01-12 00:16:14 -0800

    fix: undo previous changes

--- milestone-2/src/app/api/blogs/[slug]/route.ts
+++ milestone-2/src/app/api/blogs/[slug]/route.ts
@@ -1,40 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
-import connectDB from '../../../database/db';
-import blogSchema from '../../../database/blogSchema';
+import { NextRequest, NextResponse } from 'next/server'
+import connectDB from "../../../database/db"
+import blogSchema from "../../../database/blogSchema"
 
-export async function GET(req: NextRequest, { params }: { params: Record<string, string> }) {
-    await connectDB(); // Connect to the database
-    const { slug } = params; // Destructure slug from params
-
-    try {
-        const blog = await blogSchema.findOne({ slug }).orFail();
-        return NextResponse.json(blog);
-    } catch (err) {
-        return NextResponse.json('Blog not found.' + ' Error: ' + err, { status: 404 })
-    }
+type IParams = {
+		params: {
+			slug: string
+		}
 }
 
+// If { params } looks confusing, check the note below this code block
+export async function GET(req: NextRequest, { params }: IParams) {
+    await connectDB() // function from db.ts before
+		const { slug } = params // another destructure
 
-
-// import { NextRequest, NextResponse } from 'next/server'
-// import connectDB from "../../../database/db"
-// import blogSchema from "../../../database/blogSchema"
-
-// type IParams = {
-// 		params: {
-// 			slug: string
-// 		}
-// }
-
-// // If { params } looks confusing, check the note below this code block
-// export async function GET(req: NextRequest, { params }: IParams) {
-//     await connectDB() // function from db.ts before
-// 		const { slug } = params // another destructure
-
-// 	   try {
-// 	        const blog = await blogSchema.findOne({ slug }).orFail()
-// 	        return NextResponse.json(blog)
-// 	    } catch (err) {
-// 	        return NextResponse.json('Blog not found.' + ' Error: ' + err, { status: 404 })
-// 	    }
-// }
+	   try {
+	        const blog = await blogSchema.findOne({ slug }).orFail()
+	        return NextResponse.json(blog)
+	    } catch (err) {
+	        return NextResponse.json('Blog not found.' + ' Error: ' + err, { status: 404 })
+	    }
+}
